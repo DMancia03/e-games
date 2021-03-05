87dddfe053b67bd91616b527d7e740a8022a4c37
Diego Mancía <62128556+DiegoSan03@users.noreply.github.com>
2021-03-04 20:37:28 -0600

    JavaScript 2.0
    - Nuevas funciones agregadas:
    Cancelar compra y Desbloquear Cantidades
    - Condiciones agregadas para controlar los errores

--- js/script_compra.js
+++ js/script_compra.js
@@ -1,71 +1,78 @@
+//Se declaran las variables
+//Precio de compra
 const PRECIO = 20;
+//Porcentaje del IVA
 const PORCENTAJE_IVA = 0.13;
 
+//Se ocupa para verificar que el campo Nombre este completo
 function nameU(inputNombre) {
+    //Se extrae el valor del nombre
     var nombre = inputNombre.value;
+    //Se extrae la label donde se colocara el nombre
     var labelNombre = document.getElementById("nombre");
+    //Se extraen unos Input que se ocuparan
     var inputPlataforma = document.getElementById("slctPlataforma");
     var inputCantidad = document.getElementById("txtCantidad");
     var inputCorreo = document.getElementById("txtCorreo");
-    var btnCerrar = document.getElementById("btnComprar");
-
+    //Se verifica que el nombre no este vacio
     if (nombre != "") {
         labelNombre.innerHTML = "Nombre: " + nombre;
+        //Se el campo correo y nombre no estan vacios, se desbloquea el campo Plataforma
         if ( nombre != "" && inputCorreo.value != "" ) {
             inputPlataforma.disabled = false;
-            inputCantidad.disabled = false;
+            desbloquearCantidadesSiPlataformaSeleccionada(inputPlataforma, inputCantidad);
         }
     }else{
         labelNombre.innerHTML = "Nombre: No ingresado";
         inputPlataforma.disabled = true;
         inputCantidad.disabled = true;
-        inputCantidad.value = 0;
-        cotizacion(cantidad);
-        btnCerrar.disabled = true;
+        cancelarCompra();
     }
 }
 
+//Se ocupa para verificar que el campo de correo este lleno
 function correoU(inputCorreo) {
+    //Se extraen los input que se ocuparan el operacion
     var labelCorreo = document.getElementById("correo");
     var inputPlataforma = document.getElementById("slctPlataforma");
     var inputCantidad = document.getElementById("txtCantidad");
     var inputNombre = document.getElementById("txtCliente");
-    var btnCerrar = document.getElementById("btnComprar");
+    //Se extrae el valor del campo correo
     var correo = inputCorreo.value;
-
+    //Se verifica que no este vacio el campo
     if (correo != "") {
         labelCorreo.innerHTML = "Correo del titular: " + correo;
-        if ( correo != "" && inputNombre != "" ) {
+        //Si los campos correo y nombres estan llenos se puede realizar el siguiente paso
+        if ( correo != "" && inputNombre.value != "" ) {
             inputPlataforma.disabled = false;
-            inputCantidad.disabled = false;
+            desbloquearCantidadesSiPlataformaSeleccionada(inputPlataforma, inputCantidad);
         }
     }else{
         labelCorreo.innerHTML = "Correo del titular: No ingresado";
         inputPlataforma.disabled = true;
         inputCantidad.disabled = true;
-        inputCantidad.value = 0;
-        cotizacion(cantidad);
-        btnCerrar.disabled = true;
+        cancelarCompra();
     }
 }
 
+//Sirve para verificar que al elegir una plataforma, se habilite la cantidad
 function disableCantidad(inputPlataforma) {
+    //Se extraen los input ha ocupar
     var cantidad = document.getElementById("txtCantidad");
-    var btnComprar = document.getElementById("btnComprar");
+    //Se extrae el valor de la plataforma
     var plataforma = inputPlataforma.value;
-
+    //Se verifica que plataforma no este vacio
     if (plataforma != "") {
         cantidad.disabled = false;
-        cantidad.value = 0;
-        cotizacion(cantidad);
+        cancelarCompra();
     }else{
         cantidad.disabled = true;
-        cantidad.value = 0;
-        cotizacion(cantidad);
-        btnComprar.disabled = true;
+        cancelarCompra();
     }
 }
 
+//+++++++++ I M P O R T A N T E ++++++++++
+//Es donde se lleva a cabo la cotizacion
 function cotizacion(inputCantidad) {
     //Se extrae la cantidad inicial
     var cantidad = inputCantidad.value;
@@ -77,6 +84,8 @@ function cotizacion(inputCantidad) {
     var inputTotal = document.getElementById("total");
     //Se extrae el boton de comprar
     var btnComprar = document.getElementById("btnComprar");
+    //Se extrae el boton de cotizar
+    var btnCotizar = document.getElementById("btnCotizar");
     //Se extrae la plataforma seleccionada
     var plataforma = document.getElementById("slctPlataforma").value;
     //Se extraen las existencias de las tiendas
@@ -115,7 +124,11 @@ function cotizacion(inputCantidad) {
                 }
             break;
             default:
-                errores = true;
+                if ( cantidad == 0 ) {
+                    errores = false;
+                }else{
+                    errores = true;
+                }
             break;
         }
     }else{
@@ -124,38 +137,74 @@ function cotizacion(inputCantidad) {
 
     if (errores) {
         alert("Error: Has ingresado cantidades no validas a la existencia.");
-        inputCantidad.value = 0;
-        cotizacion(inputCantidad);
-        btnComprar.disabled = true;
+        cancelarCompra();
     }else{
-        inputSubtotal.innerHTML = "Subtotal: " + subtotal.toFixed(2);
-        inputIva.innerHTML = "Iva aplicado: " + iva.toFixed(2);
-        inputTotal.innerHTML = "Total: " + total.toFixed(2);
-        btnComprar.disabled = false;
+        inputSubtotal.innerHTML = "Subtotal: $" + subtotal.toFixed(2);
+        inputIva.innerHTML = "Iva aplicado: $" + iva.toFixed(2);
+        inputTotal.innerHTML = "Total: $" + total.toFixed(2);
+        if ( cantidad == 0 ) {
+            btnComprar.disabled = true;
+            btnCotizar.disabled = true;
+        }else{
+            btnComprar.disabled = false;
+            btnCotizar.disabled = false;
+        }
     }
 }
 
+//Funcion para realizar la compra del producto
 function compra() {
+    //Se extraen las existencias de los productos
     var cantidadSteam = document.getElementById("existencias_steam");
     var cantidadEpic = document.getElementById("existencias_epic");
     var cantidadEshop = document.getElementById("existencias_nintendo");
     var cantidadTotal = document.getElementById("existencias_total");
+    //Se extraen las cantidades a comprar
     var Inputcantidad = document.getElementById("txtCantidad");
     var cantidad = Inputcantidad.value;
-    var texto;
+    //Se extrae cual plataforma se esta ocupando
     var plataformaSeleccionada = document.getElementById("slctPlataforma").value;
-
-    if ( plataformaSeleccionada == "Steam" ) {
-        texto = cantidadSteam.innerHTML - cantidad;
-        cantidadSteam.innerHTML = texto; 
-    }else if( plataformaSeleccionada == "Epic Game Store" ){
-        texto = cantidadEpic.innerHTML - cantidad;
-        cantidadEpic.innerHTML = texto;
-    }else if( plataformaSeleccionada == "Nintendo eShop" ){
-        texto = cantidadEshop.innerHTML - cantidad;
-        cantidadEshop.innerHTML = texto;
+    //Se declara una variable a ocupar
+    var texto;
+    //Se busca a cual plataforma comprara para poder manipular los datos y restar las existencias
+    switch ( plataformaSeleccionada ) {
+        case "Steam":
+            texto = cantidadSteam.innerHTML - cantidad;
+            cantidadSteam.innerHTML = texto;     
+        break;
+        case "Epic Game Store":
+            texto = cantidadEpic.innerHTML - cantidad;
+            cantidadEpic.innerHTML = texto;    
+        break;
+        case "Nintendo eShop":
+            texto = cantidadEshop.innerHTML - cantidad;
+            cantidadEshop.innerHTML = texto;    
+        break;
+        default:
+            break;
     }
-    Inputcantidad.value = 0;
-    cotizacion(Inputcantidad);
-    alert("Su pedido se ha registrado con exito.");
+    //Se verifica que la compra sea de un producto mayor a 0
+    if ( cantidad > 0 ) {
+        alert("Su pedido se ha registrado con exito.");
+        texto = cantidadTotal.innerHTML - cantidad;
+        cantidadTotal.innerHTML = texto;
+    }else{
+        alert("La cantidad ingresada es cero, por lo tanto no se puede realizar la compra.");
+    }
+    //Se vuelve a poner en cero la cantidad y se bloquea la opcion de comprar
+    cancelarCompra();
+}
+
+//Funcion para cancelar la compra
+function cancelarCompra() {
+    var inputCantidad = document.getElementById("txtCantidad");
+    inputCantidad.value = 0;
+    cotizacion(inputCantidad);
+}
+
+//Funcion para iniciar las cantidades si ya se escogio una plataforma
+function desbloquearCantidadesSiPlataformaSeleccionada(inputPlataforma, inputCantidad) {
+    if (inputPlataforma.value != "") {
+        inputCantidad.disabled = false;
+    }
 }
